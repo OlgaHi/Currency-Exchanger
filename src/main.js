e7858224d94704921d24171b43291d110f35bcf0
Olha Hizhytska <olgainfotech@gmail.com>
2021-02-12 14:39:01 -0800

    Gathering data correctly

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
-//import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-//import CurrencyExchange from './js/blank.js';
+//import CurrencyExchange from './js/currencyExchange.js';
+
+
+// function displayResult() {
+//   $("#result").text();
+// }
+
+$(document).ready(function() {
+  $("form#exchanger").submit(function(event) {
+    event.preventDefault();
+    let currency = $("#currency option:selected").val();
+    let number = parseInt($("#amount").val());
+    console.log(number, currency);
+    //CurrencyExchange.getExchange(currency, number)
+    //.then(function(response) {
+    // displayResult(response);
+    //});
+    
+    
+  });
+})
 
