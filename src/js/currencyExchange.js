b2d29b0bf282cf88672dbea716b446530457dcf1
Olha Hizhytska <olgainfotech@gmail.com>
2021-02-12 11:48:05 -0800

    Change for fetch request

--- src/js/currencyExchange.js
+++ src/js/currencyExchange.js
@@ -1,18 +1,14 @@
 export default class CurrencyExchange {
   static getExchange() {
-    return new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const apiURL = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(request.response);
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
         }
-      };
-
-      request.open("GET", apiURL, true);
-      request.send();
-    });
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      });
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-//import Blank from './js/blank.js';
+import CurrencyExchange from './js/blank.js';
 
-console.log("main.js is executing");
