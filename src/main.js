3a21e3385812be4eb918e4c01cd874f91f9ad9c6
Olha Hizhytska <olgainfotech@gmail.com>
2021-02-12 16:36:53 -0800

    Add api call and rate calculation

--- src/main.js
+++ src/main.js
@@ -2,23 +2,31 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-//import CurrencyExchange from './js/currencyExchange.js';
+import CurrencyExchange from './js/currencyExchange.js';
 
 
-// function displayResult() {
-//   $("#result").text();
-// }
+function displayResult(response, selectedCurrency, number) {
+  
+  const rates = Object.entries(response.conversion_rates);
+  for (const [key, value] of rates) {
+    if (key === selectedCurrency) {
+      let result1 = (number * value).toFixed(2);
+      $("#result").text(result1);
+    }
+  }
+  
+}
 
 $(document).ready(function() {
   $("form#exchanger").submit(function(event) {
     event.preventDefault();
-    let currency = $("#currency option:selected").val();
+    let selectedCurrency = $("#currency option:selected").val();
     let number = parseInt($("#amount").val());
-    console.log(number, currency);
-    //CurrencyExchange.getExchange(currency, number)
-    //.then(function(response) {
-    // displayResult(response);
-    //});
+    
+    CurrencyExchange.getExchange()
+      .then(function(response) {
+        displayResult(response, selectedCurrency, number);
+      });
     
     
   });
