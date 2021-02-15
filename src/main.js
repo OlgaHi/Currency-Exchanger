5e52116d9bb15e0426a96a8a8e41896140390bb5
Olha Hizhytska <olgainfotech@gmail.com>
2021-02-15 00:31:33 -0800

    Add error handler in case of server error

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ function clearFields() {
   $('#currency_not_selected').text("");
   $('#wrong_number').text("");
   $("#result").text("");
+  $("#server_error").text("");
 
 }
 
@@ -20,12 +21,12 @@ function displayResult(response, selectedCurrency, number) {
     for (const [key, value] of rates) {
       if (key === selectedCurrency) {
         let result1 = (number * value).toFixed(2);
-        $("#result").text(`${result1}`);
-        break;
+        $("#result").text(`${number} USD (US Dollars) = ${result1} ${selectedCurrency}`);
       }
     }
+  } else {
+    $("#server_error").text(`Sorry! ${response["error-type"]}`);
   }
-  
 }
 
 $(document).ready(function() {
@@ -34,15 +35,15 @@ $(document).ready(function() {
     let selectedCurrency = $("#currency option:selected").val();
     let number = parseInt($("#amount").val());
     clearFields();
-    if (number > 0 & selectedCurrency !== '- Select currency -') {
+    if (number > 0 & selectedCurrency !== "- Select currency -") {
       CurrencyExchange.getExchange()
         .then(function(response) {
           displayResult(response, selectedCurrency, number);
         });
     } else if(selectedCurrency === '- Select currency -') {
-      $('#currency_not_selected').text("Please select available currency");
+      $("#currency_not_selected").text("Please select available currency");
     } else {
-      $('#wrong_number').text("Please enter any number more than 0!");
+      $("#wrong_number").text("Please enter any number more than 0!");
     }
   });
 });
